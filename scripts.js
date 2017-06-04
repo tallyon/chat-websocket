17224b3f26d4185b4159f001eb091be242151886
Michał <michal.przybyla.mp@gmail.com>
2017-06-04 21:47:06 +0200

    Added menu, and fixed css

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 var chatWindow = document.getElementById('');
 var messageWindow = document.getElementById('');
+
+function toggleMenu() {
+  var menuDisplay = document.getElementById('menuWindow');
+  if (menuDisplay.style.visibility === 'hidden') {
+    menuDisplay.style.visibility = 'visible';
+  } else {
+    menuDisplay.style.visibility = 'hidden';
+  }
+}
