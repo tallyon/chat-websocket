b4cb72863f9814b57241be8e2ff7034aee1187b2
Michał <michal.przybyla.mp@gmail.com>
2017-06-06 23:19:39 +0200

    Added animation for settings button

--- scripts.js
+++ scripts.js
@@ -3,9 +3,12 @@ var messageWindow = document.getElementById('');
 
 function toggleMenu() {
   var menuDisplay = document.getElementById('menuWindow');
+  var rotateImage = document.getElementById('btnMenu');
   if (menuDisplay.style.display === 'block') {
+    rotateImage.style.transform = 'rotate(45deg)';
     menuDisplay.style.display = 'none';
   } else {
+    rotateImage.style.transform = 'rotate(-90deg)';
     menuDisplay.style.display = 'block';
   }
 }
