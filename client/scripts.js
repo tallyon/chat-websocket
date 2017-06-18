902e0cae54e443812544a988d8e1b12f7c999737
Tallyon <skowronski6@gmail.com>
2017-06-18 14:44:04 +0200

    Moved client to it's own folder. Added user registration and chat message sending logic.

---
+++ client/scripts.js
@@ -0,0 +1,229 @@
+// This token will be received from the server upon joining
+// Use it to identify yourself when sending messages
+var serverToken = "";
+// Displayed name of current user
+var username = "test" + Date.now().valueOf().toString();
+
+var chatWindow = document.getElementById('chatWindow');
+var messageWindow = document.getElementById('');
+var btnChatMessageSend = document.getElementById("btnMessageSend");
+var textAreaChatMessage = document.getElementById("textChatMessageField");
+
+// Create websocket connection
+const socket = new WebSocket("ws://localhost:8080", "chat");
+
+// Connection opened
+socket.addEventListener("open", function(event) {
+    console.log("Opened socket to the chat server, connecting to chat...");
+    connectToServer(socket);
+});
+
+// SEND button click function that will send message
+btnChatMessageSend.addEventListener("click", function(event) {
+    console.log("SEND btn clicked, server token:", serverToken);
+    // If there is no server token return
+    if(serverToken === "") return;
+
+    var messageBody = textAreaChatMessage.value;
+    console.log("message body:", messageBody);
+    sendMessage(socket, serverToken, messageBody);
+
+    // Clear text area
+    textAreaChatMessage.value = "";
+});
+
+function toggleMenu() {
+    var menuDisplay = document.getElementById('menuWindow');
+    var rotateImage = document.getElementById('btnMenu');
+    if (menuDisplay.style.display === 'block') {
+        rotateImage.style.transform = 'rotate(45deg)';
+        menuDisplay.style.display = 'none';
+    }
+    else {
+        rotateImage.style.transform = 'rotate(-90deg)';
+        menuDisplay.style.display = 'block';
+    }
+}
+
+/**
+ * Orchestrate connection transaction to the server.
+ * 
+ * @param {WebSocket} serverSocket WebSocket object that will be used to communicate with the server
+ * @param {function(boolean)} callback after connecting to server is completed
+ */
+function connectToServer(serverSocket, callback) {
+    // Add handler for message WebSocket event
+    serverSocket.addEventListener("message", onMessageWebSocketHandler);
+
+    // Send login request to the server to receive token
+    requestLogin(serverSocket, username);
+}
+
+/**
+ * Sends register message to the server with provided username.
+ * 
+ * @param {WebSocket} serverSocket WebSocket object connected to the server
+ * @param {string} username Username that will be registered
+ */
+function requestLogin(serverSocket, username) {
+    var registerMessage = createRegisterTransaction(username);
+
+    console.log("Sending REGISTER transaction to the server, message:", registerMessage);
+    serverSocket.send(JSON.stringify(registerMessage));
+}
+
+/**
+ * Creates REGISTER transaction payload.
+ * 
+ * @param {string} username Username that will be registered.
+ */
+function createRegisterTransaction(username) {
+    var message = {
+        type: "register",
+        timestamp: Math.floor(Date.now().valueOf() / 1000),
+        data: {
+            username: username
+        }
+    }
+
+    return message;
+}
+
+
+/**
+ * Creates CHAT transaction payload.
+ * 
+ * @param {string} userToken Token of current user
+ * @param {string} body Chat message body text
+ */
+function createChatTransaction(userToken, body) {
+
+    if(userToken == null || body == null) return null;
+
+    var message = {
+        type: "chat",
+        timestamp: Math.floor(Date.now().valueOf() / 1000),
+        data: {
+            userToken: userToken,
+            body: body
+        }
+    };
+
+    return message;
+}
+
+/**
+ * This function will handle all communication received from the server.
+ * 
+ * @param {MessageEvent} event 
+ */
+function onMessageWebSocketHandler(event) {
+    // Try to parse the event data
+    var parsedData = null;
+
+    try {
+        parsedData = JSON.parse(event.data);
+    }
+    catch(e) {
+        console.error("unable to parse server message:", event.data);
+        return;
+    }
+
+    if(parsedData == null) {
+        console.error("received empty data from server");
+        return;
+    }
+
+    if(parsedData.type == null) {
+        console.log("received message data without type from the server, omitting");
+        return;
+    }
+
+    // Get identifier from transaction message type
+    var transactionType = transactionTypeStringToNum(parsedData.type);
+
+    // Server has sent register message with user token or error
+    switch(transactionType) {
+        case 0:
+            // REGISTER
+            registerTransactionHandler(parsedData);
+            break;
+        case 1:
+            // UNREGISTER
+            break;
+        case 2:
+            // PING
+            break;
+        case 3:
+            // CHAT
+            chatTransactionHandler(parsedData);
+            break;
+        default:
+            // Unrecognized transaction type
+            return;
+    }
+};
+
+/**
+ * Handles incoming REGISTER transaction message.
+ * 
+ * @param {any} message Transaction message JSON payload.
+ */
+function registerTransactionHandler(message) {
+    console.log("Received REGISTER transaction, message:", message);
+
+    if(message.data.userToken != null) {
+        serverToken = message.data.userToken;
+    }
+}
+
+/**
+ * Handles incoming CHAT transaction message.
+ * 
+ * @param {any} message Transaction message JSON payload
+ */
+function chatTransactionHandler(message) {
+    console.log("Received CHAT transaction, message:", message);
+    chatWindow.innerHTML += "<br/>" + message.data.body;
+}
+
+/**
+ * Send chat message to the server.
+ * 
+ * @param {string} userToken User token received upon registration.
+ * @param {string} text Body of chat message to send
+ */
+function sendMessage(serverSocket, userToken, text) {
+    var chatMessage = createChatTransaction(userToken, text);
+
+    if(chatMessage == null) {
+        return;
+    }
+
+    serverSocket.send(JSON.stringify(chatMessage));
+}
+
+/**
+ * Converts string transaction message type to number identifier.
+ * Returns -1 if identifier is not recognized.
+ * 
+ * @param {string} type String identifying transaction message type
+ * @returns {number}
+ */
+function transactionTypeStringToNum(type) {
+
+    if(type == null) return -1;
+
+    switch(type.toLowerCase()) {
+        case "register":
+            return 0;
+        case "unregister":
+            return 1;
+        case "ping":
+            return 2;
+        case "chat":
+            return 3;
+        default:
+            return -1;
+    }
+}
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var WebSocketServer = require('websocket').server;
-var express = require("express");
-var app = express();
+const WebSocketServer = require('websocket').server;
+const uuidv4 = require("uuid/v4");
+const express = require("express");
+const app = express();
 
 const port = 8080;
+
+// Array with all registered users
+var registeredUsers = [];
+
+// Array with all websocket connections
+var socketConnections = [];
 
 // For each connection output current time, requesting ip and url to the console
 app.all("*", function(req, res, next) {
@@ -34,13 +41,47 @@ wsServer.on("request", function(req) {
     }
 
     var connection = req.accept("chat", req.origin);
+    // Add this websocket connection to array of all connections
+    socketConnections.push(connection);
     
     var timeNow = new Date(Date.now());
     console.log(timeNow.toUTCString(), "Connection accepted from", req.origin);
     
     connection.on("message", function(message) {
         if(message.type === "utf8") {
-            console.log("Received message:", message.utf8Data);
+            // Parse message
+            var parsedMessage = null;
+
+            try {
+                parsedMessage = JSON.parse(message.utf8Data);
+            }
+            catch(e) {
+                console.log("Could not parse incoming message");
+                return;
+            }
+
+            console.log("Received message:", parsedMessage);
+
+            var messageType = parsedMessage.type;
+
+            if(messageType == null) {
+                console.log("Invalid message type, skip message");
+                return;
+            }
+
+            switch(messageType.toLowerCase()) {
+                case "register":
+                    // REGISTER message received from the client
+                    registerTransactionHandler(connection, parsedMessage);
+                    break;
+                case "chat":
+                    // CHAT message received from the client
+                    chatTransactionHandler(connection, parsedMessage);
+                    break;
+                default:
+                    console.log("Unrecognized message type:", messageType);
+                    return;
+            }
 
             // If message was PING send message type ping with no other text
             if(message.utf8Data == "PING") {
@@ -66,7 +107,120 @@ wsServer.on("request", function(req) {
             connection.sendBytes(0);
         }
     });
+
     connection.on("close", function(reasonCode, description) {
         console.log("Peer", connection.remoteAddress, "disconnected");
     });
 });
+
+function registerTransactionHandler(connection, message) {
+    if(message == null || message.data == null || message.data.username == null) return;
+
+    var username = message.data.username;
+    console.log("Received register transaction message from the client to register username", username);
+
+    // Register new user
+    var userToken = registerUser(username);
+
+    var responseRegisterMessage = {
+        type: "register",
+        timestamp: Math.floor(Date.now().valueOf() / 1000),
+        data: null
+    };
+
+    if(userToken === "") {
+        // User has not been registered, add error message to response
+        responseRegisterMessage.error = "username taken";
+    }
+    else {
+        // User has been registered, add username and user token to response
+        responseRegisterMessage.data = {
+            username: username,
+            userToken: userToken
+        };
+    }
+
+    // Send back register transaction message to the user
+    connection.sendUTF(JSON.stringify(responseRegisterMessage));
+
+    // Send chat message to broadcast new user entering the chat
+    sendChatMessage(socketConnections, username + " has joind the chat!");
+}
+
+/**
+ * Registers new user in chat. Returns user token string when successfuly added user to chat, empty string otherwise.
+ * 
+ * @param {string} username Username of new user
+ * @return {string}
+ */
+function registerUser(username) {
+    // Check if there is user with this username registered
+    var foundUser = registeredUsers.find((user, index) => {
+        return user.username == username;
+    });
+
+    if(foundUser == null) {
+        // This username is not taken
+        var newUser = {
+            username: username,
+            userToken: uuidv4()
+        };
+
+        // Add new user to registered users array
+        registeredUsers.push(newUser);
+
+        // Return user token of registered user
+        return newUser.userToken;
+    }
+    else {
+        // This username is taken
+        return "";
+    }
+}
+
+/**
+ * Sends chat transaction message to clients.
+ * 
+ * @param {connection} connection WebSocket connection object
+ * @param {string} body Body text of chat message
+ */
+function sendChatMessage(connections, body) {
+    // Create transaction message with type chat
+    var transactionMessage = {
+        type: "chat",
+        timestamp: Math.floor(Date.now().valueOf() / 1000),
+        data: {
+            body: body
+        }
+    };
+
+    console.log("Sending chat transaction message to the clients:", transactionMessage);
+    connections.forEach((connection) => {
+        connection.sendUTF(JSON.stringify(transactionMessage));
+    });
+}
+
+function chatTransactionHandler(connection, message) {
+    if(message == null || message.data == null || message.data.userToken == null) return;
+
+    console.log("Received chat transaction message from the client to broadcast the message", message);
+
+    var userToken = message.data.userToken;
+
+    // Find username of the user in array of registered users
+    var foundUser = registeredUsers.find((user) => {
+        return user.userToken == userToken;
+    });
+
+    if(foundUser == null) {
+        // Could not find user with this token
+        return;
+    }
+    else {
+        // Found user with provided token, create chat message body to broadcast
+        var chatMessageBody = foundUser.username + ": " + message.data.body;
+
+        // Send chat message
+        sendChatMessage(socketConnections, chatMessageBody);
+    }
+}
